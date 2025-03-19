d7661cbc7b46d8f1c9c80c261682c8d3d80cfdd4
claramamute <claraflor22@outlook.com>
2025-03-19 09:54:56 -0300

    feat:Window-Created

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode(size=(600, 480))
+
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close window
+            quit() # Stop pygame
+
+
+
