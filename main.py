10c23f5a1f228e0138bb90ef6afd72f325aa6d63
claramamute <claraflor22@outlook.com>
2025-03-21 17:39:23 -0300

    feat:Connecting Menu and Game Classes

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Setup Start')
-pygame.init()
-screen = pygame.display.set_mode(size=(600, 480))
-
-print('Loop Start')
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # Close window
-            quit() # Stop pygame
-
-
-
+game = Game()
+game.run()
